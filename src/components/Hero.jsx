d7f0cb0759a043dacbdf91432d10a6ae90107a40
JimeBlue <jimeblau@gmail.com>
2026-06-11 10:47:13 +0200

    add fonts and hero headline text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { FaPlus } from "react-icons/fa";
 const Hero = () => {
   return (
     <section className="bg-pink flex flex-col items-center gap-8 py-20 px-4">
-      <h1 className="text-3xl sm:text-5xl font-bold text-center max-w-xl">
-        Lorem ipsum dolor sit amet
+      <h1 className="font-pliant text-3xl sm:text-5xl font-medium text-center max-w-xl">
+        Every day has a story. Write yours
       </h1>
-      <button className="btn btn-sm md:btn-md lg:btn-lg xl:btn-xl bg-navy border-2 border-navy text-white font-semibold px-10 rounded-full cursor-pointer flex items-center gap-2 hover:bg-transparent hover:text-navy">
+      <button className="btn btn-sm md:btn-md lg:btn-lg xl:btn-xl bg-navy border-2 border-navy font-nunito text-white font-bold px-10 rounded-full cursor-pointer flex items-center gap-2 hover:bg-transparent hover:text-navy">
         <FaPlus />
         Add Entry
       </button>
